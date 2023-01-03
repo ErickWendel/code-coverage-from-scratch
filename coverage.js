8bb1132298dff10f48bcb2f3d340020f0897c518
Erick Wendel <erick.workspace@gmail.com>
2023-01-03 13:43:51 -0300

    colorfull coverage
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- coverage.js
+++ coverage.js
@@ -9,10 +9,7 @@ function filterResult(result) {
     return path.isAbsolute(url) && url !== __filename
   })
 }
-
-function insertAt(target, index, str) {
-  return target.slice(0, index) + str + target.slice(index)
-}
+ 
 async function showMetrics() {
   // create a new inspector session
   const session = new inspector.Session();
@@ -42,28 +39,56 @@ async function showMetrics() {
     generateCoverageReport(sourceCode, coverage.functions)
 
   }
+}
 
-  function generateCoverageReport(sourceCode, coverage) {
-    // split the source code into an array of lines
-    let lines = sourceCode
-    
-    for (const fn of coverage) {
-      // if(!fn.functionName) continue;
+// function generateCoverageReport(sourceCode, coverage) {
+//   console.log(JSON.stringify(coverage, null, 2))
+//   // split the source code into an array of lines
+//   let lines = sourceCode
+//   let myLines = ''
+//   for (const fn of coverage) {
+//     if (!fn.functionName) continue;
 
-      for (const range of fn.ranges) {
-        if (range.count) continue;
+//     for (const range of fn.ranges) {
+//       if (range.count) continue;
 
-        const fullLine = lines.slice(range.startOffset, range.endOffset).trimStart()
-        console.log({ fullLine })
-        const splitted = lines.split('\n')
-        const line = splitted.findIndex((l) => l.indexOf(fullLine.replace(/\n/, '')) !== -1)
-        if(line === -1) continue;
-        splitted[line] = splitted[line].concat(' <--- //not covered!')
-        lines = splitted.join('\n')
+//       const input = lines;
+//       const output = input.slice(0, range.startOffset) +
+//         // "<---- aqui" +
+//         "\x1b[31m" + 
+//         input.slice(range.startOffset, range.endOffset) +
+//         // ">---- aqui" +
+//         "\x1b[0m" + 
+//         input.slice(range.endOffset);
+//       myLines = output
+//       break
+//     }
+//   }
+//   console.log(myLines)
+// }
+
+function generateCoverageReport(sourceCode, coverage) {
+  // split the source code into an array of lines
+  let lines = sourceCode
+  let linesSplitted = sourceCode.split('\n')
+  console.log(JSON.stringify(coverage, null, 2))
+  for (const fn of coverage) {
+    if (!fn.functionName) continue;
+
+    for (const range of fn.ranges) {
+      if (range.count) continue;
+
+      const fullLine = lines.slice(range.startOffset, range.endOffset).trimStart()
+      const str = linesSplitted.map(item => item.trimStart())
+      for (const i of fullLine.split('\n').map(item => item.trimStart())) {
+        const index = str.indexOf(i)
+        if(index == -1) continue;
+        // console.log('line', linesSplitted[index])
+        linesSplitted[index] = `\x1b[31m ${linesSplitted[index]} \x1b[0m`
       }
     }
-    console.log(lines)
   }
+  console.log(linesSplitted.join("\n"))
 }
 // start the showMetrics function
 showMetrics();
--- sum.js
+++ sum.js
@@ -1,9 +1,25 @@
 function sum(a, b) {
-  if (a > 0) {
+  if (a >= 9 && a<= 10) {
     return a + b;
   }
-  return a ++ ;
+
+  if (a == 3) {
+    return a;
+  }
+
+  if (a == 4) {
+    return a;
+  }
+
+  if (a == 6) {
+    return a + b;
+  }
+
+  return a++;
 }
 
 sum(10, 20)
-
+// sum(4, 20)
+// sum(6, 20)
+// sum(3, 20)
+// sum(20, 20)
